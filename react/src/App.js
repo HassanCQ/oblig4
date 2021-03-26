3661545b0d592d9a96b17fa4e2ea2130d15ce979
Hassanvm <77785021+Hassanvm@users.noreply.github.com>
2021-03-26 15:23:21 +0100

    Add files via upload

---
+++ react/src/App.js
@@ -0,0 +1,12 @@
+import Movies from "./components/Movies";
+
+const App = () => {
+    return (
+        <section>
+            <h1> Oblig 4 - React og Sanity </h1>
+            <Movies/>
+        </section>
+        );
+    };
+
+export default App;
